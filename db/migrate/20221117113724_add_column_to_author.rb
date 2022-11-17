52ec60a7f2f50f7c4d26a08f22d2c5ff72772887
sanjay <sanjay827985>
2022-11-17 20:07:41 +0530

    initial

---
+++ db/migrate/20221117113724_add_column_to_author.rb
@@ -0,0 +1,7 @@
+class AddColumnToAuthor < ActiveRecord::Migration[7.0]
+  def change
+    add_column :students, :address, :string
+    add_column :students, :contact_number, :string
+  
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+
+student = Student.create(
+    first_name: 'sanjay',
+    last_name: 'dwivedi',
+    email: 'skumar0443@gmail.com'
+)
+
+20.times do |i|
+    puts "creating students #{i+1}"
+    Student.create(
+        first_name: "Student #{i+1}",
+        last_name: "Student #{i+1}",
+        email:  "student#{i+1}@bootcamp.com"
+    )
+end
